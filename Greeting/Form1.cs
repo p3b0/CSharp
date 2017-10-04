00391f01c76abf4b70de0efa98bbfe1b62bbac82
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2017-10-04 09:36:03 +0200

    Greeting male or female, done.

---
+++ Greeting/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greeting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int maleOrFemale = int.Parse(textBox3.Text.Substring(9,1));
+            if ((maleOrFemale % 1) != 0)
+            {
+                MessageBox.Show("Hej! fru " + textBox2.Text + ".");
+            }
+            else
+            {
+                MessageBox.Show("Hej! herr " + textBox2.Text + ".");
+            }
+            
+        }
+    }
+}
