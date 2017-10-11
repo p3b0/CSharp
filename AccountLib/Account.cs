c52e557914cdbd521f77d7fd47745f5785c58569
Peter Borg <Peter.Borg.spo16@ecusolna.onmicrosoft.com>
2017-10-11 11:03:03 +0200

    New test, unfinished.

---
+++ AccountLib/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AccountLib
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public bool IsSummary { get; set; }
+        public List<Account> Children { get; set; }
+
+    }
+}
